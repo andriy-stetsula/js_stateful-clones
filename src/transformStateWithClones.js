05c7edc7f7782c22fa9c643303373415def786fe
Андрій <andriystetsula2344gmail.com@MacBook-Pro-Andrij.local>
2026-04-03 16:08:00 +0300

    git add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,33 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+
+  let currentState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    const { type } = action;
+
+    if (type === 'clear') {
+      currentState = {};
+    }
+
+    if (type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    }
+
+    if (type === 'removeProperties') {
+      const newState = { ...currentState };
+
+      for (const key of action.keysToRemove || []) {
+        delete newState[key];
+      }
+      currentState = newState;
+    }
+    result.push({ ...currentState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
